7f2d3e120cee52b4d2e2afdb66e273ca19b4c3ec
Calvin Bochulak <calvin.bochulak@gmail.com>
2020-12-24 00:12:28 -0700

    Update config + dev env

--- config/config.go
+++ config/config.go
@@ -7,15 +7,11 @@ import (
 )
 
 type Config struct {
-	dbName     string
-	dbHost     string
-	dbPort     string
-	dbUser     string
-	dbPswd     string
-	dbTestName string
-	dbTestHost string
-	apiPort    string
-	migrate    string
+	dbName string
+	dbHost string
+	dbPort string
+	dbUser string
+	dbPswd string
 }
 
 func Get() *Config {
@@ -26,41 +22,14 @@ func Get() *Config {
 	flag.StringVar(&cfg.dbPort, "dbport", os.Getenv("POSTGRES_PORT"), "database port")
 	flag.StringVar(&cfg.dbUser, "dbuser", os.Getenv("POSTGRES_USER"), "database user")
 	flag.StringVar(&cfg.dbPswd, "dbpswd", os.Getenv("POSTGRES_PASSWORD"), "database password")
-	flag.StringVar(&cfg.dbTestName, "dbtestname", os.Getenv("TEST_DB_NAME"), "database name")
-	flag.StringVar(&cfg.dbTestHost, "dbtesthost", os.Getenv("TEST_DB_HOST"), "database host")
-	flag.StringVar(&cfg.apiPort, "apiport", os.Getenv("API_PORT"), "API Port")
-	flag.StringVar(&cfg.migrate, "migrate", "up", "specify database migration direction {up, down}")
 
 	flag.Parse()
 
 	return cfg
 }
 
-func (c *Config) GetDBConnStr() string {
-	return c.getDBConnStr(c.dbHost, c.dbName)
-}
-
-func (c *Config) GetTestDBConnStr() string {
-	return c.getDBConnStr(c.dbTestHost, c.dbTestName)
-}
-
-func (c *Config) getDBConnStr(dbhost, dbname string) string {
-	return fmt.Sprintf(
-		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
-		c.dbUser,
-		c.dbPswd,
-		dbhost,
-		c.dbPort,
-		dbname,
-	)
-}
-
 func (c *Config) GetDSNStr() string {
 	return c.getDSNStr(c.dbHost, c.dbName)
-}
-
-func (c *Config) GetTestDSNStr() string {
-	return c.getDSNStr(c.dbTestHost, c.dbTestName)
 }
 
 func (c *Config) getDSNStr(dbhost, dbname string) string {
@@ -72,12 +41,4 @@ func (c *Config) getDSNStr(dbhost, dbname string) string {
 		c.dbUser,
 		c.dbPswd,
 	)
-}
-
-func (c *Config) GetAPIPort() string {
-	return ":" + c.apiPort
-}
-
-func (c *Config) GetMigration() string {
-	return c.migrate
 }
